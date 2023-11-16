35e2adc87df7b00ee3f48b1f824c0cc40148c53d
rrrr4444 <reedcolloton@gmail.com>
2023-11-16 13:50:04 -0600

    done?

--- src/BrailleASCIITables.java
+++ src/BrailleASCIITables.java
@@ -1,19 +1,50 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 
+/**
+ * Utilities for converting using BitTrees.
+ *
+ * @author Reed Colloton
+ */
 public class BrailleASCIITables {
+  BitTree ASCIIToBraille = new BitTree(8);
+  BitTree BrailleToASCII = new BitTree(6);
+  BitTree BrailleToUnicode = new BitTree(6);
+
+  /**
+   * Constructor
+   */
   BrailleASCIITables() {
-    BitTree brailleToASCII = new BitTree(6);
-    BitTree ASCIIToBraille = new BitTree(5);
-    BitTree UnicodeToBraille = new BitTree(16);
-    try (FileInputStream asciiFile = new FileInputStream("translations/ascii.txt");
-         FileInputStream unicodeFile = new FileInputStream("translations/ascii.txt")) {
-      brailleToASCII.load(asciiFile);
-      brailleToASCII.load(unicodeFile);
+    try (FileInputStream brailleFile = new FileInputStream("translations/ascii.txt");
+         FileInputStream asciiFile = new FileInputStream("translations/braille.txt");
+         FileInputStream unicodeFile = new FileInputStream("translations/unicode.txt")) {
+      ASCIIToBraille.load(brailleFile);
+      BrailleToASCII.load(asciiFile);
+      BrailleToUnicode.load(unicodeFile);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+  } // BrailleASCIITables
 
-  }
+  /**
+   * Converts an ASCII character to a string of bits representing the corresponding braille character..
+   */
+  String toBraille(char letter) {
+    return ASCIIToBraille.get(letter + "");
+  } // toBraille()
 
-}
+  /**
+   * Converts a string of bits representing a braille character to the corresponding ASCII character.
+   */
+  String toASCII(String bits) {
+    return BrailleToUnicode.get(bits);
+  } // toASCII()
+
+  /**
+   * Converts a string of bits representing a braille character to the corresponding Unicode braille character for those bits. You need only support six-bit braille characters.
+   */
+  String toUnicode(String bits) {
+    return BrailleToUnicode.get(bits);
+  } // toUnicode()
+
+} // class BrailleASCIITables
