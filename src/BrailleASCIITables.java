4ff366fcde9ee5d68eac936c93dad90cfef20442
rrrr4444 <reedcolloton@gmail.com>
2023-11-15 21:54:32 -0600

    start

--- src/BrailleASCIITables.java
+++ src/BrailleASCIITables.java
@@ -1,3 +1,19 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class BrailleASCIITables {
+  BrailleASCIITables() {
+    BitTree brailleToASCII = new BitTree(6);
+    BitTree ASCIIToBraille = new BitTree(5);
+    BitTree UnicodeToBraille = new BitTree(16);
+    try (FileInputStream asciiFile = new FileInputStream("translations/ascii.txt");
+         FileInputStream unicodeFile = new FileInputStream("translations/ascii.txt")) {
+      brailleToASCII.load(asciiFile);
+      brailleToASCII.load(unicodeFile);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
 
 }
