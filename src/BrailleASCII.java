9039a47179d79b59c83e06aeb61dab922152d96e
rrrr4444 <reedcolloton@gmail.com>
2023-11-16 13:49:42 -0600

    start

--- src/BrailleASCII.java
+++ src/BrailleASCII.java
@@ -1,2 +1,19 @@
+/**
+ * CLI translator.
+ *
+ * @author Reed Colloton
+ */
 class BrailleASCII {
-}
+  public static void main(String[] args) {
+    if (args.length != 2) {
+      throw new IllegalArgumentException();
+    } // if
+    BrailleASCIITables tables = new BrailleASCIITables();
+    switch (args[0].toLowerCase()) {
+      case "braille" -> tables.toBraille(args[1]);
+      case "ascii" -> tables.toASCII(args[1]);
+      case "unicode" -> tables.toUnicode(args[1]);
+    } // switch
+  } // main()
+
+} // class BrailleASCII
